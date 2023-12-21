55566189ca64a8c1524486dbfda18ea3ef88b888
monet-joe <20459298+monet-joe@users.noreply.github.com>
2023-12-21 13:33:44 +0800

    upl chest falsetto base code

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
-# ccmusic_clstask_eval
+# Bel-Folk-Classification
+[![Python application](https://github.com/monet-joe/ccmusic_clstask_eval/actions/workflows/python-app.yml/badge.svg?branch=genre)](https://github.com/monet-joe/ccmusic_clstask_eval/actions/workflows/python-app.yml)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/monet-joe/ccmusic_clstask_eval/blob/genre/LICENSE)
 
-[![Python application](https://github.com/MuGeminorum/ccmusic_clstask_eval/actions/workflows/python-app.yml/badge.svg?branch=main)](https://github.com/MuGeminorum/ccmusic_clstask_eval/actions/workflows/python-app.yml)
+Classify singing method by fine-tuned pre-trained CNN models.
 
-Evaluation for all classification tasks among CCMUSIC
+## Requirements
+```bash
+conda create -n cnn --yes --file conda.txt
+conda activate cnn
+pip install -r requirements.txt
+```
+
+## Usage
+### Code download
+```bash
+git clone -b chest-falsetto https://github.com/monet-joe/ccmusic_clstask_eval.git
+cd ccmusic_clstask_eval
+```
+
+### Train
+Assign a backbone(take squeezenet1_1 as an example) after `--model` to start training:
+```bash
+python train.py --model squeezenet1_1 --fullfinetune True
+```
+`--fullfinetune True` means full finetune, `False` means linear probing
+
+<a href="https://www.modelscope.cn/datasets/monetjoe/cv_backbones/dataPeview" target="_blank">Supported backbones</a> 
+
+### Plot results
+After finishing the training, use below command to plot latest results:
+```bash
+python plot.py
+```
+
+### Predict
+Use below command to predict an audio target by latest saved model:
+```bash
+python eval.py --target ./test/example.wav
+```
+
+## Cite
+```bash
+@dataset{zhaorui_liu_2021_5676893,
+  author       = {Zhaorui Liu, Monan Zhou, Shenyang Xu, Zhaowen Wang, Wei Li and Zijin Li},
+  title        = {CCMUSIC DATABASE: A Music Data Sharing Platform for Computational Musicology Research},
+  month        = {nov},
+  year         = {2021},
+  publisher    = {Zenodo},
+  version      = {1.1},
+  doi          = {10.5281/zenodo.5676893},
+  url          = {https://doi.org/10.5281/zenodo.5676893}
+}
+```
---
+++ model.py
@@ -0,0 +1,186 @@
+import os
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from datasets import load_dataset
+from utils import url_download, create_dir, model_dir
+
+
+def get_backbone(ver, backbone_list):
+    for bb in backbone_list:
+        if ver == bb['ver']:
+            return bb
+
+    print('Backbone name not found, using default option - alexnet.')
+    return backbone_list[0]
+
+
+def model_info(backbone_ver):
+    backbone_list = load_dataset(
+        path="monet-joe/cv_backbones",
+        split="IMAGENET1K_V1"
+    )
+    backbone = get_backbone(backbone_ver, backbone_list)
+    m_type = str(backbone['type'])
+    input_size = int(backbone['input_size'])
+    m_url = str(backbone['url'])
+
+    return m_type, input_size, m_url
+
+
+def download_model(pre_model_url):
+    pre_model_path = model_dir + '/' + (pre_model_url.split('/')[-1])
+    create_dir(model_dir)
+
+    if not os.path.exists(pre_model_path):
+        url_download(pre_model_url, pre_model_path)
+
+    return pre_model_path
+
+
+def Classifier(cls_num: int, output_size: int, linear_output: bool):
+    q = (1.0 * output_size / cls_num) ** 0.25
+    l1 = int(q * cls_num)
+    l2 = int(q * l1)
+    l3 = int(q * l2)
+
+    if linear_output:
+        return torch.nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(output_size, l3),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(l3, l2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(l2, l1),
+            nn.ReLU(inplace=True),
+            nn.Linear(l1, cls_num)
+        )
+
+    else:
+        return torch.nn.Sequential(
+            nn.Dropout(),
+            nn.Conv2d(output_size, l3, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten(),
+            nn.Linear(l3, l2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(l2, l1),
+            nn.ReLU(inplace=True),
+            nn.Linear(l1, cls_num)
+        )
+
+
+class Net():
+    model = None
+    m_type = 'alexnet'
+    m_url = 'https://download.pytorch.org/models/alexnet-owt-7be5be79.pth'
+    input_size = 224
+    output_size = 512
+    training = True
+    full_finetune = False
+
+    def __init__(self, cls_num, m_ver='alexnet', saved_model_path='', full_finetune=False):
+        self.training = (saved_model_path == '')
+        self.full_finetune = full_finetune
+        self.m_type, self.input_size, self.m_url = model_info(m_ver)
+
+        if not hasattr(models, m_ver):
+            print('Unsupported model.')
+            exit()
+
+        self.model = eval('models.%s()' % m_ver)
+        linear_output = self._set_outsize()
+
+        if self.training:
+            pre_model_path = download_model(self.m_url)
+            checkpoint = torch.load(pre_model_path, map_location='cpu')
+            if torch.cuda.is_available():
+                checkpoint = torch.load(pre_model_path)
+
+            self.model.load_state_dict(checkpoint, False)
+
+            for parma in self.model.parameters():
+                parma.requires_grad = self.full_finetune
+
+            self._set_classifier(cls_num, linear_output)
+            self.model.train()
+
+        else:
+            self._set_classifier(cls_num, linear_output)
+            checkpoint = torch.load(saved_model_path, map_location='cpu')
+            if torch.cuda.is_available():
+                checkpoint = torch.load(saved_model_path)
+
+            self.model.load_state_dict(checkpoint, False)
+            self.model.eval()
+
+    def _set_outsize(self, debug_mode=False):
+        for name, module in self.model.named_modules():
+            if str(name).__contains__('classifier') or str(name).__eq__('fc') or str(name).__contains__('head'):
+                if isinstance(module, torch.nn.Linear):
+                    self.output_size = module.in_features
+                    if debug_mode:
+                        print(
+                            f"{name}(Linear): {self.output_size} -> {module.out_features}")
+                    return True
+
+                if isinstance(module, torch.nn.Conv2d):
+                    self.output_size = module.in_channels
+                    if debug_mode:
+                        print(
+                            f"{name}(Conv2d): {self.output_size} -> {module.out_channels}")
+                    return False
+
+        return False
+
+    def _set_classifier(self, cls_num, linear_output):
+        if hasattr(self.model, 'classifier'):
+            self.model.classifier = Classifier(
+                cls_num, self.output_size, linear_output)
+            return
+
+        elif hasattr(self.model, 'fc'):
+            self.model.fc = Classifier(
+                cls_num, self.output_size, linear_output)
+            return
+
+        elif hasattr(self.model, 'head'):
+            self.model.head = Classifier(
+                cls_num, self.output_size, linear_output)
+            return
+
+        self.model.heads.head = Classifier(
+            cls_num, self.output_size, linear_output)
+
+    def _get_insize(self):
+        return self.input_size
+
+    def forward(self, x):
+        if torch.cuda.is_available():
+            x = x.cuda()
+            self.model = self.model.cuda()
+
+        if self.m_type == 'googlenet' and self.training:
+            return self.model(x)[0]
+        else:
+            return self.model(x)
+
+    def parameters(self):
+        if self.full_finetune:
+            return self.model.parameters()
+
+        if hasattr(self.model, 'classifier'):
+            return self.model.classifier.parameters()
+
+        if hasattr(self.model, 'fc'):
+            return self.model.fc.parameters()
+
+        print('Classifier part not found.')
+        return self.model.parameters()
+
+    def state_dict(self):
+        return self.model.state_dict()
